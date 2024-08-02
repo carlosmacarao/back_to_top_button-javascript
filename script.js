2d33b5d0ef40d5dd35b547a9b3f2131e7c9499b3
carlosmacarao <carlosmacarao9@gmail.com>
2024-08-02 10:42:04 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+window.onscroll = function() { scrollFunction(); };
+
+function scrollFunction() {
+
+    var mybutton = document.getElementById("back-to-top");
+
+    // Se o usuário rolar mais de 20px para baixo da parte superior do documento, mostre o botão
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// Rola o documento para cima quando o usuario clicar no botão
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
